91ea119b847ac4db0b9b75a24e2429f0ef981060
DANIEL P <dpenalva@gmail.com>
2026-05-04 18:40:27 -0300

    STYLE: ajusta nomes do menu lateral

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import streamlit as st
 
-# Configuração da página
 st.set_page_config(page_title="FARC - Telebras", layout="wide")
 
-# Mapeamento apenas dos arquivos que você tem na pasta
-p1 = st.Page("pages/01_Calculo_Simples.py", title="Cálculo Simples", icon="⚖️", default=True)
-p2 = st.Page("pages/02_Calculo_Represados.py", title="Cálculo Represados", icon="🔄")
+p1 = st.Page(
+    "pages/01_Calculo_Simples.py",
+    title="Reajuste Simples",
+    icon="⚖️",
+    default=True
+)
 
-# Navegação sem o arquivo 03
+p2 = st.Page(
+    "pages/02_Calculo_Represados.py",
+    title="Reajustes Múltiplos",
+    icon="🔄"
+)
+
 pg = st.navigation({
     "Admissibilidade e Cálculo": [p1, p2]
 })
